d46f705b9a3a758ee29b53517278cdb3419daf18
章辉(zhanghui1) <33326898+Roilka@users.noreply.github.com>
2019-11-26 15:16:17 +0800

    init program

---
+++ api/src/main/java/com/roilka/roilka/question/api/RoilkaQuestionApiApplication.java
@@ -0,0 +1,13 @@
+package com.roilka.roilka.question.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoilkaQuestionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoilkaQuestionApiApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/com/roilka/roilka/question/api/RoilkaQuestionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roilka.roilka.question.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoilkaQuestionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
