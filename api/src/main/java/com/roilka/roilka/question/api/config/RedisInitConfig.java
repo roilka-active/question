1f81def20803b6e67e3382696f7e9df842390c3b
章辉(zhanghui1) <33326898+Roilka@users.noreply.github.com>
2019-12-05 20:57:16 +0800

    完善redis测试

---
+++ api/src/main/java/com/roilka/roilka/question/api/config/RedisInitConfig.java
@@ -0,0 +1,27 @@
+package com.roilka.roilka.question.api.config;
+
+import com.roilka.roilka.question.api.testbeaninit.BussinessPerson;
+import com.roilka.roilka.question.dal.entity.user.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName AppConfig
+ * @Description TODO
+ * @Author zhanghui1
+ * @Date 2019/11/29 17:29
+ **/
+@ComponentScan(basePackageClasses = BussinessPerson.class)
+@Configuration
+public class AppConfig {
+    @Bean(name = "user1")
+    public User initUser(){
+        User user = new User();
+        //user.setId(11L);
+        user.setNote("this is testbeaninit!");
+        user.setUserName("roilka");
+        return user;
+    }
+}
